1b9c69bbc821ad4599028c09143e166333f8bc70
Lara Gorito Barbosa de Souza <laragoritob@gmail.com>
2025-05-16 17:09:02 -0300

    Versão deletada

--- BD_ONG/VIEWS/Cliente_View.sql
+++
@@ -1,3 +0,0 @@
-create view Cliente_View as 
-select nome, CPF, Data_Nascimento, UF from Cliente
-WHERE UF like 'RJ';
--- BD_ONG/VIEWS/Livro_View.sql
+++
@@ -1,3 +0,0 @@
-create view Livro_View as
-select titulo, genero, nome_autor, quantidade from livro
-WHERE quantidade > 10;
