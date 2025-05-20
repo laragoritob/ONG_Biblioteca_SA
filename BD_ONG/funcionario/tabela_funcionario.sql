02469fa98f69dc450c2024882be00955695146f5
Lara Gorito Barbosa de Souza <laragoritob@gmail.com>
2025-05-20 14:07:15 -0300

    Versão 4

--- BD_ONG/funcionario/tabela_funcionario.sql
+++ BD_ONG/funcionario/tabela_funcionario.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 create table Funcionario (
     Cod_Funcionario int not null,
     Nome varchar(50) not null,
@@ -21,30 +20,7 @@ create table Funcionario (
     Senha varchar(20) not null,
     Cargo varchar(20) not null,
 	Foto_Funcionario longblob,
-=======
-create table funcionario (
-	id_funcionario int not null,
-    nome varchar (50) not null,
-    cpf varchar(15) not null,
-    sexo char (2) not null,
-    nome_responsavel varchar(50),
-    nacionalidade varchar(20) not null,
-    escolaridade varchar(40),
-    telefone varchar(20) not null,
-    rg varchar(15) not null,
-    data_nascimento date not null,
-    data_efetivacao date not null,
-    cep varchar(20) not null,
-    uf char(2) not null,
-    cidade varchar(30) not null,
-    bairro varchar(30) not null,
-    rua varchar(40) not null,
-    num_residencia int not null,
-    usuario varchar(20) not null,
-    senha varchar(20) not null,
-    cargo varchar(20) not null,
->>>>>>> 02cdef53f81af802bedbcf85b5dd4e9a07859808
-    
+
     constraint pk_funcionario primary key (id_funcionario)
 );
     
